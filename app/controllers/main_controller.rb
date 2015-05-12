096cc193ea6a3c081828feb17089a7c03844083d
Charles <whitfield.cw@gmail.com>
2015-05-11 21:03:53 -0700

    shift bower files with no success and begin routes :

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,12 @@
 get '/' do
   erb :index
 end
+
+post '/tasks' do
+  p params[:section]
+  redirect '/'
+end
+
+get '/tasks/new' do
+  erb :"partials/_new_task", locals: { section: params[:section] }
+end
